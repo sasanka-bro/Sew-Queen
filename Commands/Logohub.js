54c5f9e121fc210bc1fd453b0cb6a2ee43eb4ecc
sasanka-bro <93658348+sasanka-bro@users.noreply.github.com>
2021-12-05 15:24:22 +0530

    Update Logohub.js

--- Commands/Logohub.js
+++ Commands/Logohub.js
@@ -1,32 +1,32 @@
 /* 
- Sew Queen Whatsapp Bot                       
+ sew king Whatsapp Bot                       
                        
- Telegram: t.me/RavinduManoj
- Facebook: https://www.facebook.com/ravindu.manoj.79
+ Telegram: t.me/Yurensasanka
+ Facebook: https://www.facebook.com/bandulasasanka.com
  Licensed under the  GPL-3.0 License;
  
- Coded By Ravindu Manoj
+ Coded By Yuren Sasanka
 */ 
-let DataPack = require('sew-queen-pro');
-let SewQueen = require('sew-queen-pro/sources/dc/handler');
-let Details = require('sew-queen-pro/sources/dc/Details');
+let DataPack = require('sew-King-pro');
+let SewQueen = require('sew-King-pro/sources/dc/handler');
+let Details = require('sew-King-pro/sources/dc/Details');
 let axios = require('axios');
-let {sendMessagettp, sendMessageEmojiToPng} = require('sew-queen-pro/sources/dc/cmd/ttp')
-let {SetUPImageInSEWQUEEN} = require('sew-queen-pro/sources/dc/cmd/setimg')
-let { SendMessageImage } = require('sew-queen-pro/sources/dc/cmd/dl')
-let {sendMessagelogolist} = require('sew-queen-pro/sources/dc/cmd/TextList')
-let {sendMessagelogores, sendMessagepngres} = require('sew-queen-pro/sources/dc/cmd/textmaker')
-let WorkType = Details.WORKTYPE == 'public' ? false : true
-var { FancyText, fancyList } = require("fancy-sew-amdi")
-let { MessageType, Mimetype } = require('@ravindu01manoj/sew-queen-web');
+let {sendMessagettp, sendMessageEmojiToPng} = require('sew-King-pro/sources/dc/cmd/ttp')
+let {SetUPImageInSEWKING} = require('sew-king-pro/sources/dc/cmd/setimg')
+let { SendMessageImage } = require('sew-king-pro/sources/dc/cmd/dl')
+let {sendMessagelogolist} = require('sew-king-pro/sources/dc/cmd/TextList')
+let {sendMessagelogores, sendMessagepngres} = require('sew-king-pro/sources/dc/cmd/textmaker')
+let WorkType = Details.WORKTYPE == 'private' ? false : true
+var { FancyText, fancyList } = require("fancy-sew-king")
+let { MessageType, Mimetype } = require('@sasanka-bro/sew-king-web');
 var LOGODISC = '';
 var des = '';
 if (Details.LANG == 'SI') {
    des = 'ඉමෝජි පින්තූර බවට පත් කරයි'
-   LOGODISC = '350 කට අදික ඌ ලෝගො සෑදීම සදහා යොදා ගන්න.අනිවාරයෙන් වචන දෙකක් යෙදිය යුතු අතර වචන දෙක / මගින් වෙන් කරන්න.\n🎲උදා:- .textlogo SEW / QUEEN'
+   LOGODISC = '350 කට අදික ඌ ලෝගො සෑදීම සදහා යොදා ගන්න.අනිවාරයෙන් වචන දෙකක් යෙදිය යුතු අතර වචන දෙක / මගින් වෙන් කරන්න.\n🎲උදා:- .textlogo SEW / KING'
 } else {
    des = "You Can Png From Any Emoji"
-   LOGODISC = '350+ Text To Image and Logo Maker... Need Two Words And Split Them Using /\neg : .textlogo SEW / Queen '
+   LOGODISC = '350+ Text To Image and Logo Maker... Need Two Words And Split Them Using /\neg : .textlogo SEW / king '
 }
 SewQueen['IntroduceCMD']({
         pattern: 'attp ?(.*)', 
@@ -42,7 +42,7 @@ SewQueen['IntroduceCMD']({
         disc: des
         }, 
 (async (QueenSew, input) => { 
-await sendMessageEmojiToPng(QueenSew, input)
+await sendMessageEmojiToPng(kingSEW, input)
 })); 
 SewQueen['IntroduceCMD']({
         pattern: 'textlogo ?(.*)', 
@@ -50,23 +50,23 @@ SewQueen['IntroduceCMD']({
         disc: LOGODISC
         }, 
 (async (QueenSew, input) => { 
-await sendMessagelogolist(QueenSew, input)
-await sendMessagelogores(QueenSew, input)
+await sendMessagelogolist(kingSEW, input)
+await sendMessagelogores(kingSEW, input)
 })); 
 SewQueen['IntroduceCMD']({
         pattern: 'fancy ?(.*)', 
         fromMe: WorkType, 
         disc: '118+ Fancy Text Maker With Unlimited Access'
         }, 
-(async (QueenSew, input) => { 
+(async (kingSEW, input) => { 
 if(input[1].includes('//--//')) {
 var text = input[1].split('//--//')[1]
 var type = input[1].split('//--//')[0]
 var fancy = await FancyText(text)
-await QueenSew.client.sendMessage(QueenSew.jid, fancy[type], MessageType.text)
+await kingSEW.client.sendMessage(kingSEW.jid, fancy[type], MessageType.text)
 } else {
 var list = await fancyList(input[1])
-await QueenSew.client.sendMessage(QueenSew.jid, list, MessageType.listMessage)
+await kingSEW  .client.sendMessage(QueenSew.jid, list, MessageType.listMessage)
 }
 }));
 
@@ -75,27 +75,27 @@ SewQueen['IntroduceCMD']({
         fromMe: true, 
         dontAdCommandList: true
         }, 
-(async (QueenSew, input) => { 
- if (QueenSew.reply_message === false || QueenSew.reply_message.image === false) return await QueenSew.client.sendMessage(QueenSew.jid,'Reply To Any Image| image size < 100kb\n\n100kb වලට අඩු ඕනෑම රූපයකට රිප්ලයි ලෙස යොදන්න..',MessageType.text);
+(async (KingSEW, input) => { 
+ if (QueenSew.reply_message === false || KingSEW.reply_message.image === false) return await QueenSew.client.sendMessage(QueenSew.jid,'Reply To Any Image| image size < 100kb\n\n100kb වලට අඩු ඕනෑම රූපයකට රිප්ලයි ලෙස යොදන්න..',MessageType.text);
 try {
- await SetUPImageInSEWQUEEN(QueenSew, input)
+ await SetUPImageInKingSEW(QueenSew, input)
  } catch (e) {
   if(e.message.includes('display')) {
-     return await QueenSew.client.sendMessage(QueenSew.jid,'Your Imgbb APIKEY is invalid.. please add the api key ( api.imgbb.com )',MessageType.text)
+     return await KingSEW.client.sendMessage(QueenSew.jid,'Your Imgbb APIKEY is invalid.. please add the api key ( api.imgbb.com )',MessageType.text)
      } else {
    return await QueenSew.client.sendMessage(QueenSew.jid,'Do Not Use Bot Here.. This Is Your Log Number',MessageType.text)
    }
   }
 })); 
 // about me
-SewQueen['IntroduceCMD']({
+KingSEW['IntroduceCMD']({
             pattern: 'codeby', 
             fromMe: true, 
             dontAdCommandList: true
             },
  (async (message, input) => {
-            var codeby = ` ✬ ᴀʙᴏᴜᴛ ʙᴏᴛ\n\nNAME    : SEW QUEEN\nVERSION : ${Details.VERSION}\nBASED ON: NODEJS / JAVASCRIPT / TYPESCRIPT\nLANGUAGE: SINHALA / ENGLISH\nON      : GITHUB\nLINK    : github.com/ravindu01manoj/Sew-Queen\nWA WEB  : @ravindu01manoj/sew-queen-web (npm)\nDOCKER  : ravindu01manoj/sewqueen:lovegift\n\n✬ ᴀʙᴏᴜᴛ ᴍᴇ \n\nNAME    : RAVINDU MANOJ\nCOUNTRY : SRI LANKA\nDISTRICT: POLONNARUWA\nZIP CODE: 51031\nAGE     : 20\nTG      : t.me/RavinduManoj\nYOUTUBE : https://youtube.com/c/TechToFuture\nGMAIL   : manojravindu66@gmail.com\nGITHUB  : github.com/ravindu01manoj`
-            var imagesew = await axios.get('https://i.ibb.co/LNvYVkn/ce30bd75cb0e.png', { responseType: 'arraybuffer' })
+            var codeby = ` ✬ ᴀʙᴏᴜᴛ ʙᴏᴛ\n\nNAME    : KING SEW\nVERSION : ${Details.VERSION}\nBASED ON: NODEJS / JAVASCRIPT / TYPESCRIPT\nLANGUAGE: SINHALA / ENGLISH\nON      : GITHUB\nLINK    : github.com/sasanka-bro/KingSEW\nWA WEB  : @sasanka-bro/king-sew-web (npm)\nDOCKER  : YurenSasanka/KingSEW:lovegift\n\n✬ ᴀʙᴏᴜᴛ ᴍᴇ \n\nNAME    : YUREN SASANKA\nCOUNTRY : SRI LANKA\nDISTRICT: NUWARAELIYA\nZIP CODE: 0222\nAGE     : 13\nTG      : t.me/Yurensasanka\nYOUTUBE : https://youtube.com/c/SasankaBroTECHlk\nGMAIL   : yurensasanka@gmail.com\nGITHUB  : github.com/sasanka-bro`
+            var imagesew = await axios.get('https://i.ibb.co/zRtbGFK/KING-SEW-LOGO-Made-with-Poster-My-Wall.jpg', { responseType: 'arraybuffer' })
             await SendMessageImage(message,Buffer.from(imagesew.data) ,'```' + codeby + '```')
 }));
 
